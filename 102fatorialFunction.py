5cc2e5c5688261bf4df047afc4e2d4a6a50111fa
Douglas Lima Benevides <51888076+douglaslimab@users.noreply.github.com>
2021-02-11 18:08:06 +0000

    functions

---
+++ 102fatorialFunction.py
@@ -0,0 +1,15 @@
+def fator(num, show = False):
+    '''
+    -> Getting the fatorial of a number.
+    :param num: Enter the number.
+    :param show: (OPTIONAL) to check the numbers
+    :return: result
+    '''
+    result = 1
+    for i in range(0, num):
+        if show == True:
+            print(f'{num - i} ', end='')
+        result *= (num - i)
+    return result
+print(fator(4, show=Tx'rue))
+# help(fator)
